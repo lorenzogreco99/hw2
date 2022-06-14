678603dcfa2a1eac59d7c366d3333f4838ddefe1
lorenzogreco99 <100862765+lorenzogreco99@users.noreply.github.com>
2022-06-14 19:53:01 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <title>Homework2</title>
-<link href="https://fonts.googleapis.com/css?family=Pangolin:400,700|Proxima+Nova" rel="stylesheet" >
+<link href="https://fonts.googleapis.com/css?family=Pangolin:400,700|Proxima+Nova">
 <meta charset="utf-8">
 <link rel="stylesheet" href="css/home.css">
 <meta name="viewport" content="width=device width, initial scale=1">
